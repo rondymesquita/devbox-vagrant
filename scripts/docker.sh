4889864ebdab6849aa28d1949fd12c68a18a7097
rondymesquita <rondinellimesquitas@gmail.com>
2020-06-08 14:16:01 -0300

    Using user name from variable

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+source ./_variables.sh
 
+
+# Docker
 sudo apt-get update
 
 sudo apt-get install -y \
@@ -20,11 +23,10 @@ sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 sudo groupadd docker -f
-USER_VAGRANT="vagrant"
-sudo usermod -aG docker $USER_VAGRANT
+sudo usermod -aG docker $BOX_USER
 
 
-# Compose
+# Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
